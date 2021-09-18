75c05082f66933a58f20be7bec435b0d6bc5b51c
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2021-09-18 13:08:28 -0400

    rafinement du prototype et ajout de la logique de changement de page

--- app/src/component/js/FicheVisite.js
+++ app/src/component/js/FicheVisite.js
@@ -8,23 +8,41 @@ import AdresseFormulaire from "./AdresseFormulaire";
   sont gardées dans le component FicheVisite.
 */
 export default function FicheVisite() {
-  //Contient les différentes pages du formulaire
+  //Tableau des pages du formulaire
+  const pages = [<IdentiteFormulaire />, <AdresseFormulaire />];
   //dictionnaire contenant les informations du formulaire au complet
   const [info, setInfo] = useState({});
-  //la page courante
+  //le numéros dans le tableau 'pages' de la page courante
   const [page, setPage] = useState(0);
 
+  function changerPage(numPage) {
+    console.log(numPage);
+    setPage(numPage);
+  }
   //Il faudrait avoir une façon de se déplacer entre les différentes parties du formulaire. Peut-être un bouton
   //'suivant' et 'précédent' ainsi qu'un "menu" indiquant la progression dans le formulaire et avec lequel l'utilisateur
   //pourrait se déplacer aux différentes pages du formulaire.
   return (
     <div className="fichevisite-main">
       <h3>Fiche de Visite</h3>
-      {page === 0 && <IdentiteFormulaire />}
-      {page === 1 && <AdresseFormulaire />}
-      <button class="btn btn-suivant" onClick={() => setPage(1)}>
-        Suivant
-      </button>
+      {pages[page]}
+      <div className="boutons-footer">
+        {/* bouton pour passer à la page précédente */}
+        <button
+          className="btn btn-precedent"
+          onClick={() => changerPage(page - 1)}
+        >
+          Précedent
+        </button>
+        {/* bouton pour passer à la page suivante */}
+
+        <button
+          className="btn btn-suivant"
+          onClick={() => changerPage(page + 1)}
+        >
+          Suivant
+        </button>
+      </div>
     </div>
   );
 }
