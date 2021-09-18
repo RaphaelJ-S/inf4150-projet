d07c426079851e102dc4e34c2742ab882cc21d07
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2021-09-18 10:21:38 -0400

    ajout d'un prototype du fonctionnement de l'application

--- app/src/component/js/FicheVisite.js
+++ app/src/component/js/FicheVisite.js
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import IdentiteFormulaire from "./IdentiteFormulaire";
+import AdresseFormulaire from "./AdresseFormulaire";
 
 /* Ce component s'occupe de l'affichage des différentes partie du formulaire
 
@@ -7,18 +8,23 @@ import React, {useState} from "react";
   sont gardées dans le component FicheVisite.
 */
 export default function FicheVisite() {
+  //Contient les différentes pages du formulaire
   //dictionnaire contenant les informations du formulaire au complet
-  const [info, setInfo] = useState({
+  const [info, setInfo] = useState({});
+  //la page courante
+  const [page, setPage] = useState(0);
 
-  })
-
-
-//Il faudrait avoir une façon de se déplacer entre les différentes parties du formulaire. Peut-être un bouton
-//'suivant' et 'précédent' ainsi qu'un "menu" indiquant la progression dans le formulaire et avec lequel l'utilisateur
-//pourrait se déplacer aux différentes pages du formulaire.
+  //Il faudrait avoir une façon de se déplacer entre les différentes parties du formulaire. Peut-être un bouton
+  //'suivant' et 'précédent' ainsi qu'un "menu" indiquant la progression dans le formulaire et avec lequel l'utilisateur
+  //pourrait se déplacer aux différentes pages du formulaire.
   return (
-    <div className="main-fiche">
+    <div className="fichevisite-main">
       <h3>Fiche de Visite</h3>
+      {page === 0 && <IdentiteFormulaire />}
+      {page === 1 && <AdresseFormulaire />}
+      <button class="btn btn-suivant" onClick={() => setPage(1)}>
+        Suivant
+      </button>
     </div>
   );
 }
---
+++ app/src/component/js/IdentiteFormulaire.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function IdentiteFormulaire() {
+  return (
+    <div className="formulaire-body">
+      <h3>Formulaire Identite</h3>
+    </div>
+  );
+}
