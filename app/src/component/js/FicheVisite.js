709e02fdecc097bd610a85d6c863f9747ed09387
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2021-09-18 09:34:07 -0400

    ajouts de commentaire pour exipliquer un peu l'idée de l'application

--- app/src/component/js/FicheVisite.js
+++ app/src/component/js/FicheVisite.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
+
+/* Ce component s'occupe de l'affichage des différentes partie du formulaire
+
+  Les vérifications des champs sont effectuées dans les parties respectives mais les informations 
+  sont gardées dans le component FicheVisite.
+*/
 export default function FicheVisite() {
+  //dictionnaire contenant les informations du formulaire au complet
+  const [info, setInfo] = useState({
+
+  })
+
+
+//Il faudrait avoir une façon de se déplacer entre les différentes parties du formulaire. Peut-être un bouton
+//'suivant' et 'précédent' ainsi qu'un "menu" indiquant la progression dans le formulaire et avec lequel l'utilisateur
+//pourrait se déplacer aux différentes pages du formulaire.
   return (
     <div className="main-fiche">
       <h3>Fiche de Visite</h3>
