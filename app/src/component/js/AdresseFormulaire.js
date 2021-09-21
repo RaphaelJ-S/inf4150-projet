d11371d8bab586709dbd1d262975c05d8705dc5c
yanleclerc <ea091041@ens.uqam.ca>
2021-09-21 19:28:15 -0400

    Suggestion en commentaires

--- app/src/component/js/AdresseFormulaire.js
+++ app/src/component/js/AdresseFormulaire.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function AdresseFormulaire() {
   return (
+      /*
+      Prix demandé
+      Évaluation Municipale
+      Montant de la taxe scolaire
+      Montant de la taxe municipale
+      Construction? 
+       */
     <div className="formulaire-body">
       <h1>Formulaire adresse</h1>
     </div>
--- app/src/component/js/IdentiteFormulaire.js
+++ app/src/component/js/IdentiteFormulaire.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function IdentiteFormulaire() {
   return (
+      /*
+      Adresse
+      Courtier ou vendeur
+      Téléphone
+      Cellulaire
+      Courriel
+       */
     <div className="formulaire-body">
       <h3>Formulaire Identite</h3>
     </div>
